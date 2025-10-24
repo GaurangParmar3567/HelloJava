423e755d9b107e799a9d1b8a807b089db528e36a
GaurangParmar3567 <gaurang.190410107060@gmail.com>
2025-10-24 14:30:39 +0530

    Create gaurang.java

---
+++ gaurang.java
@@ -0,0 +1,9 @@
+
+import java.util.Date;
+
+public class gaurang {
+    public static void main(String[] args) {
+        System.out.println("Hello, Gaurang!");
+        System.out.println(new Date().toString());
+    }
+}
