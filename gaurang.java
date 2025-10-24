b9c57e2de40f1a2d44444bc51d0265e172d7dfc4
GaurangParmar3567 <gaurang.190410107060@gmail.com>
2025-10-24 14:42:51 +0530

    Update gaurang.java

--- gaurang.java
+++ gaurang.java
@@ -5,5 +5,6 @@ public class gaurang {
     public static void main(String[] args) {
         System.out.println("Hello, Gaurang!");
         System.out.println(new Date().toString());
+        System.out.println("This line added by Gaurang.");
     }
 }
